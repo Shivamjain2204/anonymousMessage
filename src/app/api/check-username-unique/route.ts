8b2a2d1700f5da76ee304e50262e9b005f479708
Shivam <jainshivam2204@gmail.com>
2024-08-27 23:09:38 +0530

    addition in unique username logic

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -27,7 +27,21 @@ export async function GET(request: Request){
             }, {status: 400})
         }
 
+        const {username} = result.data
+
+        const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true})
         
+        if (existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: "username is already taken",
+            }, {status: 400})
+        }
+
+        return Response.json({
+            success: true,
+            message: "username is unique",
+        }, {status: 400})
 
     } catch (error) {
         console.error("Error checking username", error);
